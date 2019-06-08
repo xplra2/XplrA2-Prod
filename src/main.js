ccdfe90e936d3fadbebd43d1b6611ae130a2d45a
sletap <sletap@umich.edu>
2019-06-08 19:41:32 -0400

    add firebase

--- src/main.js
+++ src/main.js
@@ -4,6 +4,19 @@ import router from './router'
 import store from './store'
 import './registerServiceWorker'
 import '../node_modules/bulma/css/bulma.css'
+import firebase from "firebase";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAyDf8SscMFHHtxIP35NSVqkfbxTrEVCaM",
+  authDomain: "xplra2.firebaseapp.com",
+  databaseURL: "https://xplra2.firebaseio.com",
+  projectId: "xplra2",
+  storageBucket: "xplra2.appspot.com",
+  messagingSenderId: "522767696585",
+  appId: "1:522767696585:web:9131abc3e8dd0172"
+};
+
+firebase.initializeApp(firebaseConfig)
 
 Vue.config.productionTip = false
 
@@ -12,3 +25,4 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,28 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button class='button' v-on:click='createUser'>Click me to create a user</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import firebase from 'firebase';
 
 export default {
   name: 'home',
   components: {
     HelloWorld
-  }
+  },
+  methods: {
+    createUser: function(){
+      console.log("clicked function")
+      firebase.auth().createUserWithEmailAndPassword('test1@umich.edu', "thisismypassword1234")
+      .catch(function() {
+        console.log("invalid signup credentials"); 
+      });
+    }
+  },
 }
 </script>
